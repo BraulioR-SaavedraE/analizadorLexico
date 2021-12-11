0575ddd9094425e6c75f7ed1a6894b25380f7180
BraulioR-SaavedraE <braulio@ciencias.unam.mx>
2021-12-11 00:28:45 -0600

    Agregando archivos correctos

---
+++ src/tokens.h
@@ -0,0 +1,54 @@
+#ifndef TOKENS_H
+#define TOKENS_H
+
+#include <map>
+#include <string>
+
+using namespace std;
+
+namespace C_1
+{
+    
+    const int CHAR = 1;
+    const int INT = 2;
+    const int FLOAT = 3;
+    const int DOUBLE = 4;
+    const int ID =5;
+    const int COMA = 6;
+    const int STRUCT = 7;
+    const int PUNTO_COMA = 8;
+    const int VOID = 9;
+    const int IF = 10;
+    const int ELSE =11;
+    const int LBRA = 12;
+    const int RBRA = 13;
+    const int WHILE = 14;
+    const int DO = 15;
+    const int POINT = 16;
+    const int PRINT = 17;
+    const int SCAN = 18;
+    const int BREAK = 19;
+    const int RETURN = 20;
+    const int NUMERO = 21;
+    const int AND = 22;
+    const int OR = 23;
+    const int MAYOR = 24;
+    const int MENOR = 25;
+    const int IGUAL = 26;
+    const int MAYOROI = 27;
+    const int MENOROI = 28;
+    const int MAS = 29;
+    const int MENOS = 30;
+    const int ASIG = 31;
+    const int MUL = 32;
+    const int DIV = 33;
+    const int ESP = 34;
+    const int CARACTER = 35;
+    const int CADENA = 36;
+    const int LPAR = 37;
+    const int RPAR = 38;
+    const int NEGACION = 39;
+    const int DIFERENCIA = 40;
+} // namespace C_1
+
+#endif
